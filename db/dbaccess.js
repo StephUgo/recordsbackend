0b5b1b871dd9a9dd32121c83e386d848497c6106
Steph Ugo <remo@neuf.fr>
2020-02-20 23:03:53 +0100

    Retrieve dbURL from configuration file. Update Dockerfile to set the NODE_ENV variable.

--- db/dbaccess.js
+++ db/dbaccess.js
@@ -1,13 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
+var config = require('config');
 
 // Modified to Work dockerized MongoDb : use localhost instead of database if you're not using the dockerized MongoDb AND calling from
 // a dockerized node app, e.g. with monk it looks like this :
 //var dbRecords = monk('database:27017/recordcollectiondb',null,null);
 //var dbRecords = monk('localhost:27017/recordcollectiondb',null,null);
+//const uri = 'mongodb://database:27017/';
+console.log("process.env.NODE_ENV  = " + process.env.NODE_ENV);
+const uri = config.get('dbUrl');
+console.log("dbUrl  = " + uri);
 
 // When running the node app inside a container and accessing the mongo DB running on the host which has launched the node app container,
 // we use the hostname 'host.docker.internal'
-const uri = 'mongodb://host.docker.internal:27017/';
+//const uri = 'mongodb://host.docker.internal:27017/';
 
 let _db;
 
