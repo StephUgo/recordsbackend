06b5727ed76e8c770ec5937e741e8634779c7922
Steph Ugo <remo@neuf.fr>
2020-05-13 22:40:44 +0200

    Fix http return code when authorization fails.

--- auth/middleware/auth.service.js
+++ auth/middleware/auth.service.js
@@ -18,6 +18,6 @@ module.exports = function(req, res, next) {
     next();
   } catch (ex) {
     // Invalid token
-    res.status(400).send("Invalid token.");
+    res.status(401).send("Invalid token.");
   }
 };
