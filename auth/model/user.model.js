6e36241486942d011419ea4a634302272ac8605f
StephUgo <ugolini.stephane@neuf.fr>
2023-11-27 22:12:37 +0100

    Fix code after Joi upgrade

--- auth/model/user.model.js
+++ auth/model/user.model.js
@@ -53,13 +53,13 @@ const User = mongoose.model('User', UserSchema);
 
 // Function to validate user 
 function validateUser(user) {
-  const schema = {
+  const schema = Joi.object({
     name: Joi.string().min(3).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(3).max(255).required()
-  };
+  });
 
-  return Joi.validate(user, schema);
+  return schema.validate(user);
 }
 
 exports.User = User; 
