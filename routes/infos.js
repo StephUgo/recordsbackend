046fbf26e2d1b58d48f89785aa0d2ab38a4128f1
StephUgo <60326865+StephUgo@users.noreply.github.com>
2023-11-30 12:38:37 +0100

    Add specific RaterLimiter in infos.js

--- routes/infos.js
+++ routes/infos.js
@@ -4,8 +4,17 @@ var router = express.Router();
 // Load MongoDB utils
 const MongoDBAccess = require('../db/dbaccess');
 
+// set up rate limiter: maximum of 30 requests per minute
+const RateLimit = require('express-rate-limit');
+const infosLimiter = RateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 300, // max requests per windowMs 
+  message: "Keep quiet, maybe get a life instead of spamming the api.",
+  headers: true
+});
+
 /* GET infos page. */
-router.get('/', function (req, res, next) {
+router.get('/', infosLimiter, function (req, res, next) {
   const db = MongoDBAccess.getDB();
 
   var collection = db.collection('records');
